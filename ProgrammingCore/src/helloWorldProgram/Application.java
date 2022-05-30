c28daa9f509c2ed856c6e226d4b098bafedc6cc1
Mixtaike <mixtaike@gmail.com>
2022-05-30 15:30:26 -0300

    A Hello World Program

---
+++ ProgrammingCore/src/helloWorldProgram/Application.java
@@ -0,0 +1,12 @@
+package helloWorldProgram;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// para imprimir a mensagem pulando linha  no final
+		System.out.println("Hello World");
+
+	}
+
+}

0ef49d5b91c1a175d8db72a3f8b2ec521e84c266
Mixtaike <mixtaike@gmail.com>
2022-05-30 15:54:16 -0300

    Using variables

---
+++ ProgrammingCore/src/usingVariables/UsingVariables.java
@@ -0,0 +1,25 @@
+package usingVariables;
+
+public class UsingVariables {
+
+	public static void main(String[] args) {
+		
+		//variaveis são como caixas, onde é possivel armazenar valores.
+		// há caixa de varios tamanhos e tipos no Java.
+		
+		// Primeira maneira de declarar uma varivel e atribuir valor
+		int myNumber;
+		
+		myNumber = 88;
+		
+		System.out.println(myNumber);
+		
+		
+		// Segunda maneira de declarar uma varivel e atribuir valor
+		
+		int myNumber2 = 88;
+		
+		System.out.println(myNumber2);
+	}
+
+}
